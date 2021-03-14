446bdff45c8df7118a865babe797a1d1c8b30015
jflaviojf3 <jflaviojf3@hotmail.com>
2021-03-14 15:43:01 -0300

    Curso completo de django

---
+++ core/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
+# Create your models here.
+
+class Evento (models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    local_evento = models.CharField(max_length=200, blank=True, null=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y - %Hh%Mm')
+
+    def get_local_evento(self):
+        if self.local_evento is None:
+            return ""
+        else:
+            return self.local_evento
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
